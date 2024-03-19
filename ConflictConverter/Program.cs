372bc75a1e8ee6d49204dd024644c3debb81aca9
DmitryZubarev <justposeur.pr@gmail.com>
2024-03-19 16:50:05 +0500

    Вынес запуск приложения в отдельный проект

--- ConflictConverter/Program.cs
+++ ConflictConverter/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConflictConverter.Domain.Abstractions;
+using ConflictConverter.Domain.Enums;
+using ConflictConverter.Domain.Services;
+using System.Data;
+
+ReadService readService = new ReadService();
+ConvertService convertService  = new ConvertService();
+OutputService outputService  = new OutputService();
+
+IReader reader = readService.GetReader(TypesToReadEnum.FromLocalJson);
+IWriter writer = outputService.GetWriter(TypesToWriteEnum.ToLocalJson);
+
+var data = reader.Read();
+var convertedData = convertService.Convert(data);
+writer.OutputData(convertedData);
+
+Console.ReadKey();
